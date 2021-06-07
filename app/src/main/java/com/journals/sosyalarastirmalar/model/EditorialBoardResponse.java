3a830a5b4edc16e1c2658cd173dc08774c4631f4
suresh <ksuresh.unique@gmail.com>
2021-06-07 11:46:10 +0530

    Initial commit

---
+++ app/src/main/java/com/journals/sosyalarastirmalar/model/EditorialBoardResponse.java
@@ -0,0 +1,153 @@
+package com.journals.sosyalarastirmalar.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class EditorialBoardResponse {
+
+
+    @SerializedName("editorialboardcnt")
+    private int editorialboardcnt;
+    @SerializedName("editorialboardarr")
+    private List<EditorialboardarrBean> editorialboardarr;
+
+    public int getEditorialboardcnt() {
+        return editorialboardcnt;
+    }
+
+    public void setEditorialboardcnt(int editorialboardcnt) {
+        this.editorialboardcnt = editorialboardcnt;
+    }
+
+    public List<EditorialboardarrBean> getEditorialboardarr() {
+        return editorialboardarr;
+    }
+
+    public void setEditorialboardarr(List<EditorialboardarrBean> editorialboardarr) {
+        this.editorialboardarr = editorialboardarr;
+    }
+
+    public static class EditorialboardarrBean {
+        @SerializedName("ename")
+        private String ename;
+        @SerializedName("e_qlf")
+        private String eQlf;
+        @SerializedName("editor_desig")
+        private String editorDesig;
+        @SerializedName("editor_dept")
+        private String editorDept;
+        @SerializedName("photo")
+        private String photo;
+        @SerializedName("editor_url")
+        private String editorUrl;
+        @SerializedName("biography")
+        private String biography;
+        @SerializedName("research_interest")
+        private String researchInterest;
+        @SerializedName("country_name")
+        private String countryName;
+        @SerializedName("editor_type")
+        private String editorType;
+        @SerializedName("uname")
+        private String uname;
+        @SerializedName("ex_unv_name")
+        private String exUnvName;
+
+        public String getEname() {
+            return ename;
+        }
+
+        public void setEname(String ename) {
+            this.ename = ename;
+        }
+
+        public String getEQlf() {
+            return eQlf;
+        }
+
+        public void setEQlf(String eQlf) {
+            this.eQlf = eQlf;
+        }
+
+        public String getEditorDesig() {
+            return editorDesig;
+        }
+
+        public void setEditorDesig(String editorDesig) {
+            this.editorDesig = editorDesig;
+        }
+
+        public String getEditorDept() {
+            return editorDept;
+        }
+
+        public void setEditorDept(String editorDept) {
+            this.editorDept = editorDept;
+        }
+
+        public String  getPhoto() {
+            return photo;
+        }
+
+        public void setPhoto(String photo) {
+            this.photo = photo;
+        }
+
+        public String getEditorUrl() {
+            return editorUrl;
+        }
+
+        public void setEditorUrl(String editorUrl) {
+            this.editorUrl = editorUrl;
+        }
+
+        public String getBiography() {
+            return biography;
+        }
+
+        public void setBiography(String biography) {
+            this.biography = biography;
+        }
+
+        public String getResearchInterest() {
+            return researchInterest;
+        }
+
+        public void setResearchInterest(String researchInterest) {
+            this.researchInterest = researchInterest;
+        }
+
+        public String getCountryName() {
+            return countryName;
+        }
+
+        public void setCountryName(String countryName) {
+            this.countryName = countryName;
+        }
+
+        public String getEditorType() {
+            return editorType;
+        }
+
+        public void setEditorType(String editorType) {
+            this.editorType = editorType;
+        }
+
+        public String getUname() {
+            return uname;
+        }
+
+        public void setUname(String uname) {
+            this.uname = uname;
+        }
+
+        public String getExUnvName() {
+            return exUnvName;
+        }
+
+        public void setExUnvName(String exUnvName) {
+            this.exUnvName = exUnvName;
+        }
+    }
+}
---
+++ app/src/main/java/com/journals/sosyalarastirmalar/network/Api.java
@@ -0,0 +1,66 @@
+package com.journals.sosyalarastirmalar.network;
+
+import com.google.gson.JsonObject;
+import com.journals.sosyalarastirmalar.model.AbstractResponse;
+import com.journals.sosyalarastirmalar.model.ArchiveResponse;
+import com.journals.sosyalarastirmalar.model.CategoryResponse;
+import com.journals.sosyalarastirmalar.model.ContactResponse;
+import com.journals.sosyalarastirmalar.model.CurrentIssueResponse;
+import com.journals.sosyalarastirmalar.model.EditorialBoardResponse;
+import com.journals.sosyalarastirmalar.model.InPressResponse;
+import com.journals.sosyalarastirmalar.model.JournalHomeResponse;
+import com.journals.sosyalarastirmalar.model.JournalsListResponse;
+import com.journals.sosyalarastirmalar.model.VolumeIssueResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface Api {
+
+
+
+    @Headers("Content-Type: application/json")
+    @POST("journalslistapi.php")
+    Call<CategoryResponse> getCategoryList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("aboutjournalapi.php")
+    Call<JournalHomeResponse> getJournalHomeDetails(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("current-issueapi.php")
+    Call<CurrentIssueResponse> getCurrentIssueList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("inpressapi.php")
+    Call<InPressResponse> getInPressList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("archiveapi.php")
+    Call<ArchiveResponse> getArchiveList(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("abstractdisplayapi.php")
+    Call<AbstractResponse> getAbstractDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("vol_issueapi.php")
+    Call<VolumeIssueResponse> getVolumeIssueDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("contactpagejournalsapi.php")
+    Call<JournalsListResponse> getJournalListDisplay(@Body JsonObject jsonObject);
+
+
+    @Headers("Content-Type: application/json")
+    @POST("contactapi.php")
+    Call<ContactResponse> getContactDisplay(@Body JsonObject jsonObject);
+
+    @Headers("Content-Type: application/json")
+    @POST("editorialboardapi.php")
+    Call<EditorialBoardResponse> getEditorialList(@Body JsonObject jsonObject);
+
+
+}
